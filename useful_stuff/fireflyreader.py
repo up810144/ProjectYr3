315329c4769bbcdcf78e5330b853f51d602f0645
Zak <up810144@myport.ac.uk>
2019-03-20 10:25:51 +0000

    updating firefly reader

--- useful_stuff/fireflyreader.py
+++ useful_stuff/fireflyreader.py
@@ -3,7 +3,7 @@ import numpy as np
 from astropy.io import fits
 from matplotlib import pyplot as plt
 
-file_path = '/users/zak/Documents/output/FireFly_eBOSS/z_0.4_0.5/CB_C2_SLAQ/spFly-z0405_CBC2SLAQ_V1_DAP.fits'
+file_path = '/users/zak/Documents/output/FireFly_eBOSS/z_0.5_0.6/CB_C6_D/spFly-z0506_CBC6D_V3_DAP.fits'
 
 # Check that the file actually exists
 
@@ -26,3 +26,12 @@ plt.ylabel("flux (1e-17erg/cm^2/s/Ang)")
 plt.xlabel("wavelength (Angstrom)")
 
 plt.show()
+
+
+'''
+calculate residule chi squared value by difference between data minus model dividied by the data (modulus of that). take median of results 
+
+
+plot metallicity and age  (light weighted) for each colour/redshift bin against vdisp (centre of bin)
+
+'''
